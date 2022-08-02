d54467a582a4f931cc2d3d268b5e1eb6dd448fbb
charliebudd <charles.budd@kcl.ac.uk>
2022-08-02 13:49:37 +0100

    adding long description

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
-
 import versioneer
+
+with open("README.md") as file:
+    long_description = file.read()
 
 setup(
     name='ecadataset',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Python loader for the Endoscopic Content Area (ECA) dataset.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Charlie Budd',
     author_email='charles.budd@kcl.ac.uk',
     url='https://github.com/charliebudd/eca-dataset',
